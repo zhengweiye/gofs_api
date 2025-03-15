25c67a759ce5bcb57e07d9141f4256ed073f18f8
郑伟业 <286128576@qq.com>
2025-03-15 20:51:02 +0800

    DustbinService的分页列表新增fileType字段

--- DustbinPojo.go
+++ DustbinPojo.go
@@ -4,6 +4,7 @@ type DustbinListVo struct {
 	Id         string `json:"id"`
 	FileName   string `json:"fileName"`
 	FileMd5    string `json:"fileMd5"`
+	FileType   string `json:"fileType"`
 	ServerName string `json:"serverName"`
 	StorePath  string `json:"storePath"`
 	FilePath   string `json:"filePath"`
